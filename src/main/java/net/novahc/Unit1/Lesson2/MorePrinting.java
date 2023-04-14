a88e50d58b3ff04d5070d8d762698ef691d0ac8f
skylarrohter <srohter@waprep.org>
2023-04-14 13:57:18 -0700

    Lesson 2 (pt. 1)

---
+++ src/main/java/net/novahc/Unit1/Lesson2/MorePrinting.java
@@ -0,0 +1,14 @@
+package net.novahc.Unit1.Lesson2;
+
+public class MorePrinting {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Hello" + "World!");
+
+        int number = 365;
+
+        System.out.println("There are "+ number +" days in a year!");
+
+        //There are 26 letters in the alphabet, and 10 numbers!
+    }
+}
